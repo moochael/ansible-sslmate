79c8b17f055c579430ab138db387b7494fce1813
David Macbale <david.macbale@act-on.net>
2019-02-25 13:30:43 -0800

    Saving progress so I can continue this at the office

--- sslmate.py
+++ sslmate.py
@@ -1,38 +1,105 @@
-#!/usr/local/bin/python3
+#!/usr/bin/python
 
-# openssl req -new -sha256 -key example.com.key -out example.com.csr
+ANSIBLE_METADATA = {
+    'metadata_version': '1.1',
+    'status': ['preview'],
+    'supported_by': 'community'
+}
 
+DOCUMENTATION = '''
+---
+module: sslmate
+
+short_description: Purchase, renew, revoke, and download a TLS certificate using the SSLMate API.
+
+version_added: "2.7"
+
+description:
+    - "This module takes credentials for an existing SSLMate (© 2019 Opsmate, Inc.)
+      account and can be used to purchase a new certificate, renew an existing
+      certificate, download an existing certificate from the account, or revoke
+      an existing certificate all via the SSLMate API."
+
+options:
+    common_name:
+        description:
+            - "Description here"
+        required: true
+    type:
+        description:
+            - "Description here"
+        required: true
+
+author:
+    - David Macbale (@moochael)
+'''
+
+EXAMPLES = '''
+
+'''
+
+RETURN = '''
+
+'''
+
+from ansible.module_utils.basic import AnsibleModule
 import requests
 
-api_key = ""
-common_create = "certs/example.com/"
-common_buy = "certs/example.com/buy"
+def run_module():
+    # define available arguments/parameters a user can pass to the module
+    module_args = dict(
+        name=dict(type='str', required=True),
+        new=dict(type='bool', required=False, default=False)
+    )
 
-base_api_endpoint = "https://sandbox.sslmate.com/api/v2/{}"
-create_api_endpoint = base_api_endpoint.format(common_create)
-buy_api_endpoint = base_api_endpoint.format(common_buy)
+    # seed the result dict in the object
+    # we primarily care about changed and state
+    # change is if this module effectively modified the target
+    # state will include any data that you want your module to pass back
+    # for consumption, for example, in a subsequent task
+    result = dict(
+        changed=False,
+        original_message='',
+        message=''
+    )
 
-with open("/Users/davidmacbale/repos/ansible-sslmate/example.com.csr") as csr_file:
-    csr_data = csr_file.read()
-cert_data = {'csr': csr_data, 'approval_method': 'email', 'approver_email': 'example@example.com' }
+    # the AnsibleModule object will be our abstraction working with Ansible
+    # this includes instantiation, a couple of common attr would be the
+    # args/params passed to the execution, as well as if the module
+    # supports check mode
+    module = AnsibleModule(
+        argument_spec=module_args,
+        supports_check_mode=True
+    )
 
-# r1 = requests.get(api_endpoint, auth=(api_key,""))
-# print(r1.json())
+    # if the user is working with this module in only check mode we do not
+    # want to make any changes to the environment, just return the current
+    # state with no modifications
+    if module.check_mode:
+        return result
 
-# r2
-# curl https://sslmate.com/api/v2/certs/example.com \
-#    -u 123_sampleapikey: \
-#    --data-urlencode csr=$'-----BEGIN CERTIFICATE REQUEST-----...' \
-#    -d approval_method=email \
-#    -d approver_email=webmaster@example.com \
+    # manipulate or modify the state as needed (this is going to be the
+    # part where your module will do what it needs to do)
+    result['original_message'] = module.params['name']
+    result['message'] = 'goodbye'
 
-r2 = requests.post(create_api_endpoint, data=cert_data, auth=(api_key,""))
+    # use whatever logic you need to determine whether or not this module
+    # made any modifications to your target
+    if module.params['new']:
+        result['changed'] = True
 
-r3 = requests.post(buy_api_endpoint, auth=(api_key,""))
+    # during the execution of the module, if there is an exception or a
+    # conditional state that effectively causes a failure, run
+    # AnsibleModule.fail_json() to pass in the message and the result
+    if module.params['name'] == 'fail me':
+        module.fail_json(msg='You requested this to fail', **result)
 
-request_list = [r2, r3]
-def request_loop(rlist):
-    for r in rlist:
-        print(r.json())
+    # in the event of a successful module execution, you will want to
+    # simple AnsibleModule.exit_json(), passing the key/value results
+    module.exit_json(**result)
 
-request_loop(request_list)
+def main():
+    run_module()
+
+if __name__ == '__main__':
+    main()
