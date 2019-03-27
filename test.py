dd79c8c05548d970397459a11f29984db97681bc
David Macbale <david.macbale@act-on.net>
2019-03-27 13:57:05 -0700

    Update gitignore

--- test.py
+++ test.py
@@ -1,21 +1,10 @@
 #!/usr/bin/python
 
+import secrets
 import requests
-from ansible.module_utils.basic import AnsibleModule
+#from ansible.module_utils.basic import AnsibleModule
 
-#sslmate_config_file = "~/.sslmate"
 sslmate_config_file = "/Users/david.macbale/.sslmate"
-
-lines = []
-with open(sslmate_config_file) as in_file:
-    for line in in_file:
-        lines.append(line)
-
-for each in lines:
-    print(each, end='')
-
-# ops sandbox api key
-api_key = ""
 
 domain = "davidmacbale.com"
 common_create = "certs/" + domain + "/"
@@ -25,18 +14,18 @@ base_api_endpoint = "https://sandbox.sslmate.com/api/v2/{}"
 create_api_endpoint = base_api_endpoint.format(common_create)
 buy_api_endpoint = base_api_endpoint.format(common_buy)
 
-# file_to_open = "openssl/" + domain + ".csr"
-# with open('%s' % file_to_open) as csr_file:
-#     csr_data = csr_file.read()
-# cert_data = {'csr': csr_data, 'approval_method': 'dns'}
+file_to_open = "openssl/" + domain + ".csr"
+with open('%s' % file_to_open) as csr_file:
+    csr_data = csr_file.read()
+cert_data = {'csr': csr_data, 'approval_method': 'dns'}
 
-# r2 = requests.post(create_api_endpoint, data=cert_data, auth=(api_key,""))
-# r3 = requests.post(buy_api_endpoint, auth=(api_key,""))
+r2 = requests.post(create_api_endpoint, data=cert_data, auth=(secrets.api_key,""))
+r3 = requests.post(buy_api_endpoint, auth=(secrets.api_key,""))
 
-# request_list = [r2, r3]
-# def request_loop(rlist):
-#     for r in rlist:
-#         print(r.json())
-#         print("\n")
+request_list = [r2, r3]
+def request_loop(rlist):
+    for r in rlist:
+        print(r.json())
+        print("\n")
 
-# request_loop(request_list)
+request_loop(request_list)
