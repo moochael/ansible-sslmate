ae7194dfefe0f8a615dbb506c8be87dad04cae4d
David Macbale <david.macbale@act-on.net>
2019-03-29 13:27:02 -0700

    Saving progress

--- test.py
+++ test.py
@@ -5,64 +5,61 @@ import requests
 import json
 #from ansible.module_utils.basic import AnsibleModule
 
-sslmate_config_file = "/Users/david.macbale/.sslmate"
+debug = True
+
 certificate_object_common_name = "davidmacbale.com"
 base_api_endpoint = "https://sandbox.sslmate.com/api/v2/{}"
 
-common_create_uri = "certs/" + certificate_object_common_name + "/" # https://sandbox.sslmate.com/api/v2/certs/davidmacbale.com/
-common_buy_uri = "certs/" + certificate_object_common_name + "/buy" # https://sandbox.sslmate.com/api/v2/certs/davidmacbale.com/buy
-revoke_uri = "certs/" + certificate_object_common_name + "/revoke" # https://sslmate.com/api/v2/certs/COMMON_NAME/revoke
+create_api_endpoint = base_api_endpoint.format("certs/{}/".format(certificate_object_common_name))         # https://sandbox.sslmate.com/api/v2/certs/davidmacbale.com/
+buy_api_endpoint = base_api_endpoint.format("certs/{}/buy".format(certificate_object_common_name))        # https://sandbox.sslmate.com/api/v2/certs/davidmacbale.com/buy
+revoke_api_endpoint = base_api_endpoint.format("certs/{}/revoke".format(certificate_object_common_name))  # https://sslmate.com/api/v2/certs/davidmacbale.com/revoke
 
-create_api_endpoint = base_api_endpoint.format(common_create_uri)
-buy_api_endpoint = base_api_endpoint.format(common_buy_uri)
-revoke_api_endpoint = base_api_endpoint.format(revoke_uri)
-
-
-# test format method
-apiurl = "https://sandbox.sslmate.com/api/v2/{}"
-apiuri = "certs/{}/buy/"
-
-buyapiurl = apiurl.format(apiuri.format(certificate_object_common_name))
-print(buyapiurl)
-# https://sandbox.sslmate.com/api/v2/certs/davidmacbale.com/buy/
-
-
-def create_certificate_object(domain):
-    file_to_open = "openssl/" + certificate_object_common_name + ".csr"
+def create_certificate_object(domain, debug):
+    file_to_open = "openssl/" + domain + ".csr"
     with open('%s' % file_to_open) as csr_file:
         csr_data = csr_file.read()
     cert_data = {'csr': csr_data, 'approval_method': 'dns'}
 
     request = requests.post(create_api_endpoint, data=cert_data, auth=(secrets.api_key,""))
-    print(request.json())
+    pubkey_hash = request.json()['pubkey_hash']
 
-def buy_certificate(domain):
+    if debug == 1:
+        print("Create Cert Object Response JSON:")
+        print(request.json(), end="\n\n")
+
+    return(pubkey_hash)
+
+def buy_certificate(domain, debug):
     request = requests.post(buy_api_endpoint, auth=(secrets.api_key,""))
-    print(request.json())
 
-def revoke_sslmate_certificate(domain):
+    if debug == 1:
+        print("Buy Cert Response JSON:")
+        print(request.json(), end="\n\n")
+
+def revoke_sslmate_certificate(domain, debug):
     cert_data = {'all': 'true'}
     request = requests.post(revoke_api_endpoint, data=cert_data, auth=(secrets.api_key,""))
-    print(request.json())
 
-def retrive_certificate(domain):
+    if debug == 1:
+        print("Revoke Cert Response JSON:")
+        print(request.json(), end="\n\n")
 
-    request1 = requests.get(retrieve_cert_endpoint, auth=(secrets.api_key,""))
+def retrive_certificate(domain, hash, debug):
+    retrieve_cert_endpoint = base_api_endpoint.format("certs/{}/instances/pubkey_hash:{}".format(certificate_object_common_name, hash))
 
-    retreive_cert_uri = "certs/" + certificate_object_common_name + "/instances/pubkey_hash:" + json_data['pubkey_hash'] # https://sandbox.sslmate.com/api/v2/certs/davidmacbale.com/instances/pubkey_hash:[HASH]
-    retrieve_cert_endpoint = base_api_endpoint.format(retreive_cert_uri)
-    request2 = requests.get(retrieve_cert_endpoint, auth=(secrets.api_key,""))
-    json_data = json.loads(request.text)
-    #print(json_data['pubkey_hash'])
+    request = requests.get(retrieve_cert_endpoint, auth=(secrets.api_key,""))
 
-    print(request.json())
+    if debug == 1:
+        print("Retreive Cert Response JSON:")
+        print(request.json(), end="\n\n")
 
 try:
-    #create_certificate_object(certificate_object_common_name)
-    #buy_certificate(certificate_object_common_name)
-    #retrive_certificate(certificate_object_common_name)
+    pubkey_hash = (create_certificate_object(certificate_object_common_name, debug=0))
+
+    create_certificate_object(certificate_object_common_name, debug)
+    buy_certificate(certificate_object_common_name, debug)
+    retrive_certificate(certificate_object_common_name, pubkey_hash, debug)
     #revoke_sslmate_certificate(certificate_object_common_name)
-    pass
 
 except:
     pass
